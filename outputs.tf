e97c35660016f6bd899d8712c250bf9955b11026
Iris1902 <irispicis1962@gmail.com>
2025-07-03 16:39:04 -0500

    :fire: refactor: Remove unused DNS outputs and update domain_users_dns output

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,3 @@
-output "user_create_dns" {
-  value = module.user_create.lb_dns
-}
-
-output "user_read_dns" {
-  value = module.user_read.lb_dns
-}
-
-output "user_update_dns" {
-  value = module.user_update.lb_dns
-}
-
-output "user_delete_dns" {
-  value = module.user_delete.lb_dns
+output "domain_users_dns" {
+  value = module.domain_users.lb_dns
 }
